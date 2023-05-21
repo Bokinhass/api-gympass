062d1734ab148d86d4369309c87789c694b01792
Maciel <pedrohenrique_maciel@hotmail.com>
2023-05-21 09:20:43 -0300

    pick up user by id

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,8 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 {
@@ -15,9 +14,7 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 
   try
   {
-    const usersRepository = new PrismaUsersRepository
-    const authenticateUseCase = new AuthenticateUseCase(usersRepository)
-
+    const authenticateUseCase = makeAuthenticateUseCase()
 
     await authenticateUseCase.execute({
       email,
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
-import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
 
 export async function register(request: FastifyRequest, reply: FastifyReply)
 {
@@ -16,9 +15,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply)
 
   try
   {
-    const usersRepository = new PrismaUsersRepository
-    const registerUseCase = new RegisterUseCase(usersRepository)
-
+    const registerUseCase = makeRegisterUseCase()
 
     await registerUseCase.execute({
       name,
